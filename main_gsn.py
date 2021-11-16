b6d17d190d770616677dfe714d9e81846cacd403
Toyosi <toyosiola@yahoo.com>
2021-11-16 15:39:22 +0100

    1st commit

--- main_gsn.py
+++ main_gsn.py
@@ -5,9 +5,54 @@ import genson
 from genson import SchemaBuilder
 
 
-f = open('./data/data_2.json', )
-jsonData = json.load(f)
-f.close()
+jsonFile = input('Enter file name:')
+
+if str(jsonFile):
+    try:
+        f = open(f'./data/{jsonFile}.json', )
+        jsonData = json.load(f)
+        f.close()
+        output_dict = {}
+
+        for i in jsonData['message']:
+            builder = SchemaBuilder()
+            builder.add_schema(jsonData['message'][f'user'])
+            builder.add_schema({"description": ''})
+            builder.add_schema({"tag": ''})
+            builder.add_schema({"required": False})
+
+            schemaP = builder.to_schema()
+
+            with open(f"./schema/sampschema_{i}.json", "w") as outfile:
+                json.dump(schemaP, outfile)
+
+            # print(schemaP)
+            # print(schemaP.get('description'))
+            # print(schemaP.get('required'))
+
+            builder2 = SchemaBuilder()
+            builder2.add_object(jsonData['message'][f'{i}'])
+
+            schema2 = builder2.to_schema()
+
+            # print(schema2)
+            # print(schema2.get('type'))
+            # print(schema2)
+
+            output_dict[f'{i}'] = {
+                                      "type": schema2.get('type'),
+                                      "tag": schemaP.get('tag'),
+                                      "description": schemaP.get('description'),
+                                      "required": schemaP.get('required')
+                                  },
+
+            with open(f"./schema/schema_1.json", "w") as outfile:
+                json.dump(output_dict, outfile)
+    except FileNotFoundError as err:
+        print(err)
+        err = 'File not in directory'
+else:
+    print('File not in directory')
 
 
 # def get_schema():
@@ -36,41 +81,7 @@ f.close()
 # print(msg)
 
 
-output_dict = {}
-
-for i in jsonData['message']:
-    builder = SchemaBuilder()
-    builder.add_schema(jsonData['message'][f'user'])
-    builder.add_schema({"description": ''})
-    builder.add_schema({"tag": ''})
-    builder.add_schema({"required": False})
-
-    schemaP = builder.to_schema()
-
-    with open(f"./schema/sampschema_{i}.json", "w") as outfile:
-        json.dump(schemaP, outfile)
-
-    print(schemaP)
-    print(schemaP.get('description'))
-    print(schemaP.get('required'))
-
-    builder2 = SchemaBuilder()
-    builder2.add_object(jsonData['message'][f'{i}'])
-
-    schema2 = builder2.to_schema()
-
-    print(schema2)
-    print(schema2.get('type'))
-    print(schema2)
 
 
 
-    output_dict[f'{i}'] =  {
-        "type": schema2.get('type'),
-        "tag": schemaP.get('tag'),
-        "description": schemaP.get('description'),
-        "required": schemaP.get('required')
-    },
 
-    with open(f"./schema/schema_1.json", "w") as outfile:
-        json.dump(output_dict, outfile)
